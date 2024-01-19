a5bb85059225fd557ae62130cae3b285f245ac11
Wilfredo J. Molina <molina.wilfredo.j@gmail.com>
2024-01-19 08:44:03 -0800

    change module url

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ terraform {
 }
 
 module "eks-module" {
-  source = "git::https://github.com/wjmolina/eks-module.git?ref=8d6bb9b"
+  source = "git::https://github.com/wjmolina/terraform-eks.git?ref=8d6bb9b"
 
   default_tags = {
     user        = "wmolina"
