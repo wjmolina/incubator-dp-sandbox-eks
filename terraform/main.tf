54b06f68402e93f3bf3a00767d4b5c8b387c1711
Wilfredo J. Molina <molina.wilfredo.j@gmail.com>
2024-01-19 10:49:35 -0800

    debug cidr block list to string

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,7 +26,7 @@ module "eks-module" {
       from_port   = 30000
       to_port     = 32767
       protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+      cidr_blocks = "0.0.0.0/0"
     }
   ]
 
